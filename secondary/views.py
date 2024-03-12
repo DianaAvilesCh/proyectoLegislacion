0105931f68c9d646f9f3c29da697601fc1444f9c
DianaAvilesCh <69444307+DianaAvilesCh@users.noreply.github.com>
2024-03-12 12:57:41 -0500

    cosas

--- secondary/views.py
+++ secondary/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from primary.models import persona
 def dashboard(request):
     return render(request, 'dashboard.html')
 def patient(request):
     return render(request, 'patient.html')
+
