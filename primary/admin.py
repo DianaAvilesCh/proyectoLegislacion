31a7f2593c80467e525cea6131c88e24e66a16d5
dennissezambrano2017 <66701370+dennissezambrano2017@users.noreply.github.com>
2024-03-15 16:04:02 -0500

    admin

--- primary/admin.py
+++ primary/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from primary.models import persona,doctor
-from secondary.models import paciente
-# Register your models here.
-admin.site.register(persona)
-admin.site.register(doctor)
-admin.site.register(paciente)
